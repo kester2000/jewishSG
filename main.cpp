d425278fd1b405cedab234b773eba04ac530f02b
kester <kester@lindeni.com>
2023-07-18 10:47:53 +0800

    interactive main

--- main.cpp
+++ main.cpp
@@ -7,25 +7,60 @@ using ll = long long;
 
 int res[1000][N][N];
 
+void print(int mp[N][N])
+{
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << mp[i][j] << ' ';
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
+    int a, b;
     int mp[N][N];
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             mp[i][j] = 1;
         }
     }
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 6; j++) {
-            mp[i][j] = 0;
+    print(mp);
+    cout << endl;
+    while (1) {
+        cin >> a;
+        if (cin.fail()) continue;
+        if (a >= -1) {
+            cin >> b;
+            if (cin.fail()) continue;
         }
-    }
-
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            cout << mp[i][j] << ' ';
+        if (a >= 0 && a < N && b >= 0 && b < N) {
+            mp[a][b] = !mp[a][b];
+            print(mp);
+            cout << endl;
+        } else if (a == -1) {
+            int n = SGMove(mp, b, res);
+            for (int k = 0; k < n; k++) {
+                print(res[k]);
+                cout << endl;
+            }
+        } else if (a == -2) {
+            print(mp);
+            cout << SG(mp) << endl;
+        } else if (a == -3) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    mp[i][j] = 0;
+                }
+            }
+        } else if (a == -4) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    mp[i][j] = 1;
+                }
+            }
         }
-        cout << endl;
     }
     cout << getHash(mp) << endl;
     cout << SG(mp) << endl;
